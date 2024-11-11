077a1c574580cd63fb8d8ff7c207f4544f2d082b
Nico Auchli <nico.auchli@students.fhnw.ch>
2024-11-11 19:10:16 +0100

    heroku readied

---
+++ server.js
@@ -0,0 +1,9 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const app = express();
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/ready4-work'));
+app.get('/*', function(req,res) {    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));});
+// Start the app by listening on the default Heroku
+portapp.listen(process.env.PORT || 8080);
